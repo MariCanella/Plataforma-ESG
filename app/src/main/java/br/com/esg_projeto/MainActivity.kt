a2221ef7a37693ac130a3250c62f081f3382a375
MariCanella <marivinhacanella@gmail.com>
2023-03-07 18:20:02 -0300

    testando onclick

--- app/src/main/java/br/com/esg_projeto/MainActivity.kt
+++ app/src/main/java/br/com/esg_projeto/MainActivity.kt
@@ -1,11 +1,23 @@
 package br.com.esg_projeto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textSigUp = findViewById<TextView>(R.id.loginEstudante)
+
+        textSigUp.setOnClickListener {
+            val openSignUp = Intent(this, SignupActivity::class.java)
+            startActivity(openSignUp)
+        }
+
     }
+
 }
